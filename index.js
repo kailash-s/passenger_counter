7ca5eb8de58cc99766abdf4d1e1a17ae06079148
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2022-04-18 09:57:19 +0100

    implemented arithmetics

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // Similar to setting a Ruby variable, we set a variable in JS using 'let'
 
-let count = 12
+// let count = 12
 
 //We can then view this variable with the syntax below
 
-console.log(count)
+// console.log(count)
 
 // For example
 
-let myAge = 28
+// let myAge = 28
 
-console.log(myAge)
+// console.log(myAge)
+
+// We can also do arithmetics using *, / and -
+
+let count = 5 + 2
+
+console.log(count)
