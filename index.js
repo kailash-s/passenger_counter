701dce5b4d19e6856fd7d8bc3830cad67ec1d0b3
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2022-04-18 09:45:37 +0100

    learnt about Cannot access 'myAge' before initialization

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// Similar to setting a Ruby variable, we set a variable in JS using 'let'
+
 let count = 12
 
+//We can then view this variable with the syntax below
+
 console.log(count)
+
+// For example
+
+let myAge = 28
+
+console.log(myAge)
