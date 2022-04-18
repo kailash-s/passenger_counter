defff05254596fe68c44b40aedc850af1ceacb83
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2022-04-18 10:10:18 +0100

    variable exmaples for arithmetics

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 // Similar to setting a Ruby variable, we set a variable in JS using 'let'
-
-// let count = 12
-
+let count = 12
 //We can then view this variable with the syntax below
-
-// console.log(count)
-
+console.log(count)
 // For example
-
-// let myAge = 28
-
-// console.log(myAge)
+let myAge = 28
+console.log(myAge)
 
 // We can also do arithmetics using *, / and -
-
 let count = 5 + 2
-
 console.log(count)
+// This can also be done with variables
+let firstBatch = 5
+let secondBatch = 7
+let count = firstBatch + secondBatch
+console.log(count)
+// For example
+let myAge = 28
+let humanDogRatio = 7
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,4 @@
-console.log(count)
+let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log(count)
