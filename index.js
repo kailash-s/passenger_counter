52cc558ca6a8e4baa8d138265f22f03f704fc273
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2022-04-18 10:31:52 +0100

    incrementing and decrementing with example

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-// Similar to setting a Ruby variable, we set a variable in JS using 'let'
-let count = 12
-//We can then view this variable with the syntax below
+// Incrementing and decrementing
+let count = 5
+count = count + 2
+count = count + 10
+count = count - 2
 console.log(count)
-// For example
-let myAge = 28
-console.log(myAge)
 
-// We can also do arithmetics using *, / and -
-let count = 5 + 2
-console.log(count)
-// This can also be done with variables
-let firstBatch = 5
-let secondBatch = 7
-let count = firstBatch + secondBatch
-console.log(count)
 // For example
-let myAge = 28
-let humanDogRatio = 7
-let myDogAge = myAge * humanDogRatio
-console.log(myDogAge)
+let bonusPoints = 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
