8560870ff14314e2c06e17116fcea398bf73d448
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2022-04-21 06:13:27 +0100

    started on functions

--- index.js
+++ index.js
@@ -3,6 +3,17 @@
 // incremement the count variable when the button is clicked
 // change the count-el in the html to reflect the new count
 
-function increment() {
-    console.log("The button was clicked")
+// function increment() {
+//     console.log("The button was clicked")
+// }
+
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
 }
+
+countdown()
+
