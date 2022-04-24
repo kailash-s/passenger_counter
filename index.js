57e7cbb8fae33472816ff780bb36c8dfbb04b3d0
Kailash Somanathan <kailashnathan@hotmail.co.uk>
2022-04-24 10:26:30 +0100

    saving the passenger count. Added initial save button

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let countEl = document.getElementById("count-el")
-console.log(countEl)
+// console.log(countEl)
 
 // initialise the count as 0
 let count = 0
@@ -9,5 +9,12 @@ let count = 0
 function increment() {
     count = count + 1
     countEl.innerText = count
+    // console.log(count)
+}
+
+// Create a function, save(), which logs out the count when it's called
+let saveEl = document.getElementById("save-btn")
+
+function save() {
     console.log(count)
 }
